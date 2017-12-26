b9a0fee5119f1e72ae8d2ff0f2719f11b159b9fe
makerling <d7e5fbb0@opayq.com>
2017-12-26 09:03:21 +0300

    changed source variable to be relative no absolute

--- 1827_oxes_to_sfm_script.sh
+++ 1827_oxes_to_sfm_script.sh
@@ -3,10 +3,9 @@
 #run this script which runs the xml to convert .oxes to .sfm, mimmicking the TE export to SFM
 #need to install libsaxonb-java in ubuntu
 #location of folder/files to process script:
-folder="/home/matthias/NetBeansProjects/TE-Paratext-XML/public_html/oxes-text-and-notes-to-SFM-June2017"
+folder=$(pwd)
 output="output.sfm"
 source="1827-GEN-June-27-2017_test_footnotes.oxes"
-#source="1827-June-27-2017.oxes"
 xslt="TE-Paratext-XSLT_main_text_conversion_and_annotations.xsl"
 #transform .oxes to .sfm file
 saxonb-xslt -o:"$folder"/"$output" -s:"$folder"/"$source" -xsl:"$folder"/"$xslt"
